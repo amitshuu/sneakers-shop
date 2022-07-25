743643ae42f580f81acaecf0c3b65163392116b4
Amit <amitshuu@gmail.com>
2022-07-25 20:15:33 +0300

    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       <LinkContainer>
         <NavLink>
           <Link className='link' to='/'>
-            Homee
+            Homeee
           </Link>
         </NavLink>
         <NavLink>
