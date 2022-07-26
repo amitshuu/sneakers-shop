fdabba7c7e9002c13dd90f54bdef48e0df53833d
Amit <amitshuu@gmail.com>
2022-07-26 12:15:36 +0300

    .

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const apolloServer = new ApolloServer({
