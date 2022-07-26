885595c32033b21a8cf62ad86111e5ae6999e801
Amit <amitshuu@gmail.com>
2022-07-26 13:20:16 +0300

    .

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static('/client/build'));
 
 app.get('*', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const apolloServer = new ApolloServer({
