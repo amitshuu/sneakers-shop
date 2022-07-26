269f9dbb1be8e21e3c830bb9ba5b0b41eb434ba0
Amit <amitshuu@gmail.com>
2022-07-26 11:45:35 +0300

    Build changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         </NavLink>
         <NavLink>
           <Link className='link' to='/shop'>
-            Shopp
+            Shop
           </Link>
         </NavLink>
       </LinkContainer>
