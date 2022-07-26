9e82ec674fb1cbd75d875babf6bd16c293c46d12
Amit <amitshuu@gmail.com>
2022-07-26 12:59:03 +0300

    .

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(express.static('build'));
 
 app.get('*', (req, res) =>
